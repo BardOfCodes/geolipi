58d7dee49e5ffb9c996cbe4efe47f65a9d271698
bardofcodes <adityaganeshan@gmail.com>
2024-04-13 14:41:27 -0400

    Updated the default

--- geolipi/torch_compute/settings.py
+++ geolipi/torch_compute/settings.py
@@ -1,10 +1,10 @@
 from typing import List, Tuple
 class Settings:
 
-    COORD_MODE: str = "bound" # or "centered"
+    COORD_MODE: str = "centered" # or "bound"
     ROT_ORDER: Tuple[str] = ("X", "Y", "Z")
 
 
-def update_settings(coord_mode: str = "bound", rot_order: Tuple[str] = ("X", "Y", "Z")):
+def update_settings(coord_mode: str = "centered", rot_order: Tuple[str] = ("X", "Y", "Z")):
     Settings.COORD_MODE = coord_mode
     Settings.ROT_ORDER = rot_order
